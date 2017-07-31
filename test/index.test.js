0b0fd714e5209b469bf114c84ac5ba3e1691e32c
Benjamin Kroeger <benjamin.kroeger@gmail.com>
2017-07-31 11:11:18 +0200

    getBlogEntries(): include `handle` in query object

--- test/index.test.js
+++ test/index.test.js
@@ -26,9 +26,9 @@ test.before(() => (unmocked ? record() : mock()));
 test.after(() => unmocked && persist());
 
 test.cb('loads feed of posts for individual blog', (t) => {
-  const query = {};
+  const query = { handle: 'c60f3b80-4284-413e-a6b2-6eafc55f2896' };
   const options = { authType: 'basic' };
-  service.getBlogPosts('c60f3b80-4284-413e-a6b2-6eafc55f2896', query, options, (error, blogPosts) => {
+  service.getBlogPosts(query, options, (error, blogPosts) => {
     t.ifError(error);
     t.true(Array.isArray(blogPosts));
     t.is(blogPosts.length, 10);
