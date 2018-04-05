70794ec764ee814c9f6c685b4841c24682fd2899
Alex Olson <alexkellenolson@gmail.com>
2018-04-06 00:29:17 +0200

    add createBlogPost method and test.

--- test/index.test.js
+++ test/index.test.js
@@ -4,12 +4,15 @@
 
 // 3rd party modules
 import test from 'ava';
+import Promise from 'bluebird';
 
 // internal modules
 import { mock, record, persist } from './fixtures/http-mocking';
 import serviceFactory from '../lib';
 
-const { unmocked } = process.env;
+const {
+  unmocked,
+} = process.env;
 
 const serviceOptions = { defaults: {} };
 if (unmocked) {
@@ -20,17 +23,17 @@ if (unmocked) {
     },
   });
 }
-const service = serviceFactory('https://lc.gish.de/blogs/', serviceOptions);
+const service = serviceFactory(process.env.IBM_CONNECTIONS_BLOGS_URL || 'https://apps.na.collabserv.com/blogs', serviceOptions);
 
 test.before(() => (unmocked ? record() : mock()));
 test.after(() => unmocked && persist());
 
 test.cb('loads feed of posts for individual blog', (t) => {
-  const query = { handle: 'c60f3b80-4284-413e-a6b2-6eafc55f2896' };
+  const query = { handle: process.env.BLOG_HANDLE || 'fb9504c6-7317-467f-b7cb-4109ec9aee6a' };
   const options = { authType: 'basic' };
   service.getBlogPosts(query, options, (error, { totalResults, entries: blogPosts }) => {
     t.ifError(error);
-    t.is(totalResults, 78);
+    t.true(totalResults > 0);
     t.true(Array.isArray(blogPosts));
     t.is(blogPosts.length, 10);
     t.end();
@@ -39,13 +42,30 @@ test.cb('loads feed of posts for individual blog', (t) => {
 
 test.cb('loads post by id for individual blog', (t) => {
   const query = {
-    entryId: '6281f1fe-3e77-4828-8df2-c85dd99597cc',
-    handle: 'c60f3b80-4284-413e-a6b2-6eafc55f2896',
+    entryId: process.env.BLOG_ENTRY || '8afbe394-0ef9-4223-b2f9-bb7b55834e61',
+    handle: process.env.BLOG_HANDLE || 'fb9504c6-7317-467f-b7cb-4109ec9aee6a',
   };
+
   const options = { authType: 'basic' };
   service.getBlogPost(query, options, (error, blogPost) => {
     t.ifError(error);
     t.truthy(blogPost);
     t.end();
   });
+});
+
+test('create blog post in blog with given handle', async (t) => {
+  const options = { authType: 'basic' };
+  const createBlogPost = Promise.promisify(service.createBlogPost, { context: service });
+
+  const createdPost = await createBlogPost({
+    handle: process.env.BLOG_HANDLE || 'fb9504c6-7317-467f-b7cb-4109ec9aee6a',
+    post: {
+      title: `Test post ${Math.floor(Math.random() * 999999)}`,
+      summary: 'Test summary',
+      content: 'Test content',
+    },
+  }, options);
+
+  t.truthy(createdPost, 'blog post edit url should be returned as a result of the create blog post request');
 });
